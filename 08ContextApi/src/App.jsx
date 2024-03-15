4d9df16b84df30da969e6ec83a4cbb70fa19fdb7
Darshanpatil29 <darshanpatil972@gmail.com>
2024-03-15 21:37:16 +0530

    Add Context API and User Login functionality

---
+++ 08ContextApi/src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./components/Login.jsx"
+import Profile from "./components/Profile.jsx"
+import UserContextProvider from "./context/UserContextProvider.jsx"
+function App() {
+
+  return (
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
